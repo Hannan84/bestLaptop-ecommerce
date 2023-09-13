75ff3bdcc7661c15cf141f62e220e6f43a2ba416
hannan84 <hannansarkar84@gmail.com>
2023-09-13 10:47:43 +0600

    first commit

---
+++ resources/views/website/partials/footer.blade.php
@@ -0,0 +1,53 @@
+<section class="companyDescripton">
+    <div class="container">
+        <div class="main-footer">
+
+            <div class="contact-us">
+                <h4>Support</h4>
+
+                <a href="call:01703808715" class="footer-big-btn">
+                    <div class="ic"><i class="material-icons">phone</i></div>
+                    <p>9:30AM - 6:30PM</p>
+                    <h5>01703808715</h5>
+                </a>
+                <a href="call:01935701029" class="footer-big-btn">
+                    <div class="ic"><i class="material-icons">phone</i></div>
+                    <p>9:30AM - 6:30PM</p>
+                    <h5>01935701029</h5>
+                </a>
+                <a href="https://www.google.com/maps/dir//Harun+Super+Market+,+Road+No+-3,House+No+-7,+1st+Floor+,+Shop+No+-201%2F202/@23.8052127,90.2871376,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3755b8bccb440233:0x7a5bc5c56cbe753a!2m2!1d90.3695392!2d23.8052346?entry=ttu" class="store-locator-btn footer-big-btn">
+                    <div class="ic"><i class="material-icons">place</i></div>
+                    <p>Store Locator</p>
+                    <h5>Find Our Stores</h5>
+                </a>
+            </div>
+
+            <div class="about-us">
+                <h4 class="">About Us</h4>
+                <ul>
+                    <li><a href="{{ route('user.refund.policy') }}">Refund Policy</a></li>
+                    <li><a href="{{ route('user.terms.&.conditions') }}" class="ml-2">Terms & Condition</a></li>
+                </ul>
+            </div>
+            <div class="org-info">
+                <h4>Stay Connected</h4>
+                <p><b class="store-name">Best Laptop Gallery</b><br>Head Office: Harun Super Market, Road-3 House-7,
+                        1st Floor, Shop # 201/202 (Back site of shah ali Plaza) Senpara Porbota, Mirpur-10, Dhaka-1216</p>
+                <p><b>Email:</b><br><a href="mailto:bestlaptopgallery234@gmail.com">bestlaptopgallery234@gmail.com</a> </p>
+            </div>
+        </div>
+        <div class="social-footer">
+            <div class="social-links">
+                <a href="https://www.facebook.com/Bestlaptopgallery" target="_blank" rel="noopener" title="Facebook">
+                    <i class="bi bi-facebook"></i>
+                </a>
+                <a href="https://www.youtube.com/Bestlaptopgallery" target="_blank" rel="noopener" title="Youtube">
+                    <i class="bi bi-youtube"></i>
+                </a>
+                <a href="#" target="_blank" rel="noopener" title="Instagram">
+                    <i class="bi bi-instagram"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
